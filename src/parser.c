e64cc295ba3a3a392df36c4d26b7f8683e77bd13
Mohamed Agoumi <magoumi@e2r1p4.1337.ma>
2019-12-27 19:18:47 +0100

    reading all the content and saving it for now

--- src/parser.c
+++ src/parser.c
@@ -3,13 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aihya <aihya@student.1337.ma>              +#+  +:+       +#+        */
+/*   By: magoumi <magoumi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/27 11:58:57 by aihya             #+#    #+#             */
-/*   Updated: 2019/12/27 12:18:15 by aihya            ###   ########.fr       */
+/*   Updated: 2019/12/27 19:17:29 by magoumi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem-in.h"
 
+int     read_content(t_data *data)
+{
+	char	*line;
+	int		ret;
 
+	data->content = NULL;
+	ret = -1;
+	while ((ret = get_next_line(STDOUT, &line)) == 1)
+	{	
+		ft_chain_push(&(data->content),line);
+		ft_strdel(&line);
+	}
+	if (ret == -1)
+		return (0);
+	return (1);
+}
+
+int		read_all(t_data *data)
+{
+	if (!read_content(data))
+		return (0);
+	if (data->content == NULL)
+		return (0);
+	if ((data->na = ft_atoi(data->content[0])) <= 0)
+		return (0);
+	printf("ants num [%d]\n", data->na);
+	return (1);
+}
